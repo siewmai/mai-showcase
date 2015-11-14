01d63cd663382d3aaa48df6045eeffa24821662e
Siew Mai Chan <chansiewmai@gmail.com>
2015-11-15 03:29:12 +0800

    added SignIn/SignUp View

---
+++ mai-showcase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  mai-showcase
+//
+//  Created by Siew Mai Chan on 15/11/2015.
+//  Copyright © 2015 Siew Mai Chan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
---
+++ mai-showcase/MaterialButton.swift
@@ -0,0 +1,20 @@
+//
+//  MaterialButton.swift
+//  mai-showcase
+//
+//  Created by Siew Mai Chan on 15/11/2015.
+//  Copyright © 2015 Siew Mai Chan. All rights reserved.
+//
+
+import UIKit
+
+class MaterialButton: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).CGColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSizeMake(0.0, 2.0)
+    }
+}
