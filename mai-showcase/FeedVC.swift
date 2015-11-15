345e6fcf7457c8414f3cfd704157e3aab8dfb96b
Siew Mai Chan <chansiewmai@gmail.com>
2015-11-16 00:43:32 +0800

    added feeds view

---
+++ mai-showcase/FeedVC.swift
@@ -0,0 +1,33 @@
+//
+//  FeedVC.swift
+//  mai-showcase
+//
+//  Created by Siew Mai Chan on 15/11/2015.
+//  Copyright © 2015 Siew Mai Chan. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+    }
+}
---
+++ mai-showcase/PostCell.swift
@@ -0,0 +1,34 @@
+//
+//  PostCell.swift
+//  mai-showcase
+//
+//  Created by Siew Mai Chan on 15/11/2015.
+//  Copyright © 2015 Siew Mai Chan. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var showcaseImg: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func drawRect(rect: CGRect) {
+        profileImg.layer.cornerRadius = profileImg.frame.size.width / 2
+        profileImg.clipsToBounds = true
+        
+        showcaseImg.clipsToBounds = true
+        
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
